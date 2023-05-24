4f79644e73e108fab56f0d0355905682f381ce4d
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-24 22:35:20 +0700

    page1

--- pages/page1.py
+++ pages/page1.py
@@ -52,10 +52,8 @@ all_titles = [df2['title'][i] for i in range(len(df2['title']))]
 
 
 
-st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h21>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center; color: #EBB02D;'>By Yen Le</h4>", unsafe_allow_html=True)
-image = Image.open('bg.png')
-st.image(image, width=700)
+st.markdown("<h1 style='text-align: center; color: #10316B;'>Content Based Recommnder</h21>", unsafe_allow_html=True)
+st.subheader('This is where you get movies which is content-related to your reference. Enter your favorite movie\'s name and let the system do the rest!')
 
 form = st.form(key='my_form')
 user_title = form.text_input(label='Enter a movie')
