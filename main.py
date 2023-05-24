08392a219265d8a030cf524998fc2e4ec81cddbb
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-24 17:56:16 +0700

    commit 12

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 from PIL import Image
 
 
-st.markdown("<h2 style='text-align: center; color: 10316B;'>Movie Recommendation System</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: #10316B;'>Movie Recommendation System</h2>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; color: #FFE867;'>By Yen Le</h4>", unsafe_allow_html=True)
 image = Image.open('bg.png')
 st.image(image, width=650)
