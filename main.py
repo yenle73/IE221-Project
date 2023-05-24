da398634e01eb8778a3b816b257730fecf90f3bb
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-24 18:36:49 +0700

    new commit

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_recommendations(title):
     return_df = pd.DataFrame(columns=['Title','Release Year'])
     return_df['Title'] = df['title'].iloc[movie_indices]
     return_df['Release Year'] = df2['release_year'].iloc[movie_indices]
-    return_df['Similariry Score'] = [sim_scores[i][1] for i in range(10)]
+    return_df['Similarity Score'] = [sim_scores[i][1] for i in range(10)]
     return return_df
 
 st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h21>", unsafe_allow_html=True)
