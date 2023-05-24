514378765e34b0adb8069de7e9b1099db017cd69
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-24 18:22:09 +0700

    text color

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 
 st.markdown("<h2 style='text-align: center; color: #10316B;'>Movie Recommendation System</h2>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center; color: #FFE867;'>By Yen Le</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: #EBB02D;'>By Yen Le</h4>", unsafe_allow_html=True)
 image = Image.open('bg.png')
 st.image(image, width=700)
 
