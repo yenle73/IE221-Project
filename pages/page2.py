984ca48e5b8e864d05b50202df88362d4b8d4b81
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-24 23:59:06 +0700

    drop overview

--- pages/page2.py
+++ pages/page2.py
@@ -105,5 +105,6 @@ b = get_recommendations(a)
 df_genre = df_movies.loc[df_movies['genres'] == a][:5]
 df_genre_alike_1 = df_movies.loc[df_movies['genres'] == b[1]].head(3)
 df_genre_alike_2 = df_movies.loc[df_movies['genres'] == b[2]].head(2)
-results = pd.concat([df_genre, df_genre_alike_1, df_genre_alike_2]).drop(column='overview')
+results = pd.concat([df_genre, df_genre_alike_1, df_genre_alike_2])
+results = results.drop(column='overview')
 st.markdown(results.style.set_table_styles([dict(selector='*', props=[('text-align', 'center')]), dict(selector='th', props=[('min-width', '150px')])]).to_html(),unsafe_allow_html=True)
