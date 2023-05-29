7f73011214a2c16860809a210e7f505527fd7281
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-29 14:03:17 +0700

    add more text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h21>", unsafe_allow_html=True)
+st.write(':star: Discover New Movies :star:')
 st.markdown("<h4 style='text-align: center; color: #EBB02D;'>By Yen Le</h4>", unsafe_allow_html=True)
 image = Image.open('bg.png')
 st.image(image)
