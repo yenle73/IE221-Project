fe28ba59a7cafe135b92413e02039852010305dd
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-29 13:54:52 +0700

    page2 table

--- pages/page2.py
+++ pages/page2.py
@@ -106,4 +106,5 @@ if submitted:
     df_genre_alike_2 = df_movies.loc[df_movies['genres'] == b[2]].head(2)
     results = pd.concat([df_genre, df_genre_alike_1, df_genre_alike_2])
     results = results.drop('overview', axis=1)
-    st.markdown(results.style.set_table_styles([dict(selector='*', props=[('text-align', 'center')]), dict(selector='th', props=[('min-width', '150px')])]).to_html(),unsafe_allow_html=True)
+    #st.markdown(results.style.set_table_styles([dict(selector='*', props=[('text-align', 'center')]), dict(selector='th', props=[('min-width', '150px')])]).to_html(),unsafe_allow_html=True)
+    st.table(results)
