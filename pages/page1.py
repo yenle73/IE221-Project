a37eaf05e69ad1eecfe633b9f6f606b93e44035a
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-29 12:15:58 +0700

    unique key text

--- pages/page1.py
+++ pages/page1.py
@@ -62,9 +62,9 @@ st.markdown("<h5 style='text-align: center; color: #10316B;'>Enter your favorite
 #submit_button = form_1.form_submit_button(label='Submit')
 
 form_1 = st.form(key='my_form')
-user_title_1 = form_1.text_input(label='Enter a movie\'s name')
-user_title_2 = form_1.text_input(label='Enter a movie\'s name')
-user_title_3 = form_1.text_input(label='Enter a movie\'s name')
+user_title_1 = form_1.text_input(label='Enter a movie\'s name', key='ut1')
+user_title_2 = form_1.text_input(label='Enter a movie\'s name', key='ut2')
+user_title_3 = form_1.text_input(label='Enter a movie\'s name', key='ut3')
 submit_button = form_1.form_submit_button(label='Submit')
 
 
