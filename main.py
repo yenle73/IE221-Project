bb43c9dea0095f6f2fc3f018d76e54e2957217fa
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-30 15:18:49 +0700

    pages

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ add_page_title()
 
 show_pages([
     Page("main.py", "Home Page", ":home:"),
-    Page("page1.py", "Content Based Filtering", ":notebook:"),
-    Page("page2.py", "Sentiment and Genre Recommendation", ":blue_book:"),
-    Page("page3.py", "Collaborative Filtering", ":bar_chart:"),
+    Page("pages/page1.py", "Content Based Filtering", ":notebook:"),
+    Page("pages/page2.py", "Sentiment and Genre Recommendation", ":blue_book:"),
+    Page("pages/page3.py", "Collaborative Filtering", ":bar_chart:"),
 ])
