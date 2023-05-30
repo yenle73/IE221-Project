d44703df113fde7309521fa55527d551e721d246
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-30 15:29:36 +0700

    new page

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.image(image)
 add_page_title()
 
 show_pages([
-    Page("pages/main.py", "Home Page", ":house:"),
+    Page("main.py", "Home Page", ":house:"),
     Page("pages/page1.py", "Content Based Filtering", ":notebook:"),
     Page("pages/page2.py", "Sentiment and Genre Recommendation", ":blue_book:"),
     Page("pages/page3.py", "Collaborative Filtering", ":bar_chart:"),
