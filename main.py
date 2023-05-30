e6cdc6d4a9fc10a18d3d2499111c90f2a827f159
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-05-30 15:34:44 +0700

    name changing

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.image(image)
 
 show_pages([
     Page("main.py", "Home Page", ":house:"),
-    Page("pages/page1.py", "Content Based Filtering", ":notebook:"),
-    Page("pages/page2.py", "Sentiment and Genre Recommendation", ":blue_book:"),
-    Page("pages/page3.py", "Collaborative Filtering", ":bar_chart:"),
+    Page("pages/page1.py", "Content Based Recommender", ":notebook:"),
+    Page("pages/page2.py", "Mood and Genre Based Recommender", ":blue_book:"),
+    Page("pages/page3.py", "Collaborative Recommender", ":bar_chart:"),
 ])
