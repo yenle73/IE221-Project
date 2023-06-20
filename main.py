5043749673957292245fcf165833968975be64b8
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-06-21 00:18:59 +0700

    pages

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from library import library as lib
+from st_pages import Page, add_page_title, show_pages
 
 lib.st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h1> <h3 style='text-align: center; color: #10316B;'>&#10024 Discover New Movies &#10024</h3> <h5 style='text-align: center; color: #EBB02D;'>By Le Thi Kim Yen</h5> <h5 style='text-align: center; color: #EBB02D;'>Student ID: 21521695</h5>", unsafe_allow_html=True)
 image = lib.Image.open('bg.png')
 placeholder= lib.st.image(image)
+
+show_pages(
+    [
+        Page("main.py", "Home", "🏠"),
+        Page("pages/page1.py", "Content Based Recommnder", ":books:"),
+        Page("pages/page2.py", "Mood and Genre Based Recommnder", "📖"),
+        Page("pages/page3.py", "Collaborative Recommender", "✏️"),
+    ]
+)
 
 lib.st.sidebar.header("Select movie information")
 
