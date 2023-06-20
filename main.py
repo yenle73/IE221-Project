6733a602540c0e18cedf6b31aaa18f9267b25804
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-06-21 00:10:23 +0700

    more featurea

--- main.py
+++ main.py
@@ -4,4 +4,30 @@ lib.st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommend
 image = lib.Image.open('bg.png')
 lib.st.image(image)
 
+lib.st.sidebar.header("Select movie information")
+
+form = lib.st.sidebar.form("my_form")
+option = form.selectbox(
+    'Choose a genre',
+    ("Action", "Adventure", "Fantasy", "Animation", "Science Fiction", 
+     "Drama", "Thriller", "Family", "Comedy", "History", "War", "Western",
+     "Romance", "Crime", "Mystery", "Horror", "Documentary", "Music", "TV Movie", "Foreign"))
+year = form.slider("Choose release year", 1916, 2023, 2020, 1)
+submitted = form.form_submit_button("Submit")
+
+df1 = lib.pd.read_csv('data/final_data.csv')
+df_movies = df1.copy()
+
+if submitted:
+    results = df_movies[df_movies['genres'] == option]
+    results = results[results['release_year'] == year]
+    results.columns = ["Title", "Overview", "Genre", "Release Year"]
+    if results.shape[0] == 0:
+        lib.st.warning(f"There's no {option} movie released in {year} exists in database.")
+    elif results.shape[0] < 10:
+        results = results
+        lib.st.dataframe(results)
+    else: 
+        results = results.sample(n = 10)
+        lib.st.dataframe(results)
 
