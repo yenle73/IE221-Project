2da777936f2ca487bf231bde29dbe179c878eb98
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-06-21 00:31:28 +0700

    icon changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from st_pages import Page, add_page_title, show_pages
 
 show_pages(
     [
-        Page("pages/main.py", "Home", ":home:"),
+        Page("pages/main.py", "Home", "🏠"),
         Page("pages/page1.py", "Content Based Recommnder", ":books:"),
         Page("pages/page2.py", "Mood and Genre Based Recommnder", "📖"),
         Page("pages/page3.py", "Collaborative Recommender", "✏️"),
