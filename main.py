617b06e372bf644191e6298c627466afe26210c8
yenle73 <97496482+kimyen07@users.noreply.github.com>
2023-06-21 00:14:33 +0700

    placeholder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from library import library as lib
 
 lib.st.markdown("<h1 style='text-align: center; color: #10316B;'>Movie Recommendation System</h1> <h3 style='text-align: center; color: #10316B;'>&#10024 Discover New Movies &#10024</h3> <h5 style='text-align: center; color: #EBB02D;'>By Le Thi Kim Yen</h5> <h5 style='text-align: center; color: #EBB02D;'>Student ID: 21521695</h5>", unsafe_allow_html=True)
 image = lib.Image.open('bg.png')
-lib.st.image(image)
+placeholder= lib.st.image(image)
 
 lib.st.sidebar.header("Select movie information")
 
@@ -19,6 +19,7 @@ df1 = lib.pd.read_csv('data/final_data.csv')
 df_movies = df1.copy()
 
 if submitted:
+    placeholder.empty()
     results = df_movies[df_movies['genres'] == option]
     results = results[results['release_year'] == year]
     results.columns = ["Title", "Overview", "Genre", "Release Year"]
